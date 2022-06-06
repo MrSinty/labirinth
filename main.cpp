a3de2b2403a20b66f7a6f683117e7bc6682c20b9
MrSinty <107007413+MrSinty@users.noreply.github.com>
2022-06-07 02:09:41 +0300

    A Star лабиринт

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "widget.h"
+#include <QApplication>
+#include "labyrinth.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow *window = new MainWindow();
+    window->setWindowTitle("Лабиринт");
+    window->show();
+    return a.exec();
+}
